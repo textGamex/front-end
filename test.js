4bde33bab2d17ba52fd4721d594a3ace0ba6c237
textGamex <textGamex@outlook.com>
2023-10-12 17:59:57 +0800

    初始化 仓库

---
+++ test.js
@@ -0,0 +1,14 @@
+var list = {
+    type: {
+        enter(str) {
+            console.log(str);
+        }
+    },
+    name: '123'
+};
+var newList = {
+    type: 1
+};
+var method = list['type'];
+list._context = newList.type;
+console.log(list)
